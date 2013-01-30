2f1a638866b670ecbe274175cd1d66a61d922e14
Eckhart Kőppen <eck@40hz.org>
2013-01-30 22:38:36 +0200

    WIP: skip to next level change

--- demos/logic_analyzer/viewer.py
+++ demos/logic_analyzer/viewer.py
@@ -18,6 +18,13 @@ def track_mouse(event):
         canvas.coords(cursor_line, canvas.canvasx(event.x), 0, canvas.canvasx(event.x), 10000)
         cursor_pos.set("Pos: %.0fµs" % ((canvas.canvasx(event.x) * 100 /  pixel_per_microsecond)))
 
+def next_level_change(event):
+	edge = canvas.canvasx(0) + root.winfo_width()
+	for c in canvas.find_withtag("c"):
+		if canvas.itemcget(c, coords).x > edge:
+			break
+	print("Right edge: %d %.0f, item: %d" % (edge, edge * 100 / pixel_per_microsecond, c))
+
 def update_scale_factor(value):
         global pixel_per_microsecond, scale_factor
         pixel_per_microsecond = float(value)
@@ -44,7 +51,7 @@ def plot(f, x_offset, y_offset, mask, color):
 				t = -timestamp
 			x = (t + timestamp) / scale_factor + x_offset
 			canvas.create_line(x0, y0, x, y0, fill=color)
-			canvas.create_line(x, y0, x, y, fill=color)
+			canvas.create_line(x, y0, x, y, fill=color, tags = "c")
 
 frequency = 72000000.0
 update_scale_factor(1)
@@ -54,16 +61,18 @@ root = Tk()
 cursor_pos = StringVar()
 
 topframe = Frame(root)
-topframe.pack()
+topframe.pack(fill = X)
 
 scale = Scale(topframe, orient = "horizontal", length = 400, from_ = 1, to = 1000)
+filler = Label(topframe)
 cursor_label = Label(topframe, textvariable = cursor_pos)
 quit = Button(topframe, text = "Quit", command = quit)
 load = Button(topframe, text = "Reload", command = load_samples)
-scale.pack(side = LEFT, fill = X, expand = 1)
+scale.pack(side = LEFT, fill = X, padx = 10)
 cursor_label.pack(side = LEFT, padx = 10)
-quit.pack(side = RIGHT)
-load.pack(side = RIGHT, padx = 5, pady = 5)
+filler.pack(side = LEFT, expand = 1)
+quit.pack(side = LEFT)
+load.pack(side = LEFT, padx = 5, pady = 5)
 
 canvas = Canvas(root, bg = "gray", width = 800, height = 480, scrollregion = (0, 0, 800000, 480))
 canvas.pack(fill = BOTH, expand = 1)
@@ -72,7 +81,7 @@ hbar.pack(fill = X)
 hbar.config(command = canvas.xview)
 canvas.config(xscrollcommand = hbar.set)
 canvas.bind("<Motion>", track_mouse)
-
+root.bind("n", next_level_change)
 
 cursor_pos.set("Pos: %.0fµs" % 0.0)
 scale.set(pixel_per_microsecond)

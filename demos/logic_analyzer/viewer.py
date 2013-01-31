e23307b4f13b7500c59c90c06b2baa989e16a885
Eckhart Köppen <eck@40hz.org>
2013-01-31 15:55:50 +0200

    Viewer: scrolling

--- demos/logic_analyzer/viewer.py
+++ demos/logic_analyzer/viewer.py
@@ -4,15 +4,20 @@
 from Tkinter import *
 
 def load_samples():
-	global cursor_line
+	global cursor_line, last_x
+	scrollpos = canvas.canvasx(0) * 100 / pixel_per_microsecond
+	last_x = 0
 	canvas.delete(ALL)
-	cursor_line = canvas.create_line(10, 0, 10, 10000, fill = "black", dash = (4, 4))
+	cursor_line = canvas.create_line(10, 0, 10, 10000, fill = "green", dash = (4, 4))
 	f = open(sys.argv[1])
-	plot(f, 0, 50, 0x01, "red")
+	plot(f, 0, 50, 0x01, "#e00000")
 	f.close()
 	f = open(sys.argv[1])
-	plot(f, 0, 55, 0x02, "black")
+	plot(f, 0, 55, 0x02, "#a0a0a0")
 	f.close()
+	canvas.config(width = last_x)
+	canvas.config(scrollregion = (0, 0, last_x, 480))
+	canvas.xview_moveto(scrollpos / int(canvas['width']))
 
 def track_mouse(event):
         canvas.coords(cursor_line, canvas.canvasx(event.x), 0, canvas.canvasx(event.x), 10000)
@@ -20,10 +25,12 @@ def track_mouse(event):
 
 def next_level_change(event):
 	edge = canvas.canvasx(0) + root.winfo_width()
-	for c in canvas.find_withtag("c"):
-		if canvas.itemcget(c, coords).x > edge:
+	for c in canvas.find_withtag(ALL):
+		print(canvas.coords(c)[0])
+		if canvas.coords(c)[0] > edge:
 			break
 	print("Right edge: %d %.0f, item: %d" % (edge, edge * 100 / pixel_per_microsecond, c))
+	print(canvas.coords(c)[0])
 
 def update_scale_factor(value):
         global pixel_per_microsecond, scale_factor
@@ -31,7 +38,7 @@ def update_scale_factor(value):
         scale_factor = frequency / (pixel_per_microsecond * 10000.0)
 
 def plot(f, x_offset, y_offset, mask, color):
-	global canvas, timer_interval, scale_factor
+	global canvas, timer_interval, scale_factor, last_x
 	t = 0
 	x = x_offset
 	y = y_offset
@@ -50,12 +57,14 @@ def plot(f, x_offset, y_offset, mask, color):
 			if t == 0:
 				t = -timestamp
 			x = (t + timestamp) / scale_factor + x_offset
-			canvas.create_line(x0, y0, x, y0, fill=color)
-			canvas.create_line(x, y0, x, y, fill=color, tags = "c")
+			canvas.create_line(x0, y0, x, y0, fill = color, width = 2)
+			canvas.create_line(x, y0, x, y, fill = color, width = 2, tags = "c")
+	last_x = max(x, last_x)
 
 frequency = 72000000.0
 update_scale_factor(1)
 timer_interval = 0x01000000
+left_edge = 0
 
 root = Tk()
 cursor_pos = StringVar()
@@ -74,7 +83,7 @@ filler.pack(side = LEFT, expand = 1)
 quit.pack(side = LEFT)
 load.pack(side = LEFT, padx = 5, pady = 5)
 
-canvas = Canvas(root, bg = "gray", width = 800, height = 480, scrollregion = (0, 0, 800000, 480))
+canvas = Canvas(root, bg = "#003e4d", width = 800, height = 480, scrollregion = (0, 0, 800000, 480))
 canvas.pack(fill = BOTH, expand = 1)
 hbar = Scrollbar(root, orient = HORIZONTAL)
 hbar.pack(fill = X)
@@ -82,6 +91,8 @@ hbar.config(command = canvas.xview)
 canvas.config(xscrollcommand = hbar.set)
 canvas.bind("<Motion>", track_mouse)
 root.bind("n", next_level_change)
+root.bind("q", lambda e: exit())
+root.bind("r", lambda e: load_samples())
 
 cursor_pos.set("Pos: %.0fµs" % 0.0)
 scale.set(pixel_per_microsecond)
